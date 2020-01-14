01c01183198924a05b7ab8422ec37263b642bc3a
CMonk <tianlang0704@gmail.com>
2020-01-15 03:20:51 +0800

    添加返回按钮

--- Assets/Scripts/SceneFilter.cs
+++ Assets/Scripts/SceneFilter.cs
@@ -8,6 +8,7 @@ using System.IO;
 public class SceneFilter : MonoBehaviour
 {
     public GameObject buttonPrefab;
+    public GameObject backPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +25,9 @@ public class SceneFilter : MonoBehaviour
             button.transform.SetParent(gameObject.transform, false);
             Button b = button.GetComponent<Button>();
             b.onClick.AddListener(() => {
-                SceneManager.LoadScene(currentSceneName, LoadSceneMode.Single);
+                StartCoroutine(LoadSceneAsync(currentSceneName));
+                // SceneManager.LoadScene(currentSceneName, LoadSceneMode.Single);
             });
-
             var buttonText = button.GetComponentInChildren<Text>();
             buttonText.text = currentSceneName;
         }
@@ -39,5 +40,25 @@ public class SceneFilter : MonoBehaviour
     void Update()
     {
         
+    }
+
+
+    IEnumerator LoadSceneAsync(string sceneName) {
+        // 异步切换成精
+        Scene currentScene = SceneManager.GetActiveScene();
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // 切换场景后生成返回按钮
+        var newScene = SceneManager.GetSceneByName(sceneName);
+        var buttonHost = Instantiate(backPrefab);
+        buttonHost.transform.SetParent(null, false);
+        SceneManager.MoveGameObjectToScene(buttonHost, newScene);
+        
+        // 卸载当前场景
+        SceneManager.UnloadSceneAsync(currentScene);
     }
 }
